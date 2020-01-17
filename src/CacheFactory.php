a703dac2cab36ad7cf0f2eebe298f63f1ee4b1e2
四月 <dancefun@163.com>
2020-01-17 17:15:19 +0800

    代码调整

--- src/CacheFactory.php
+++ src/CacheFactory.php
@@ -12,15 +12,29 @@ class CacheFactory
 
     public static $instance = [];
 
+    private static $handlerClass = [
+        /** 文件缓存 */
+        'File'    => \denha\cache\drivers\File::class,
+        /** Mongodb缓存 */
+        'Mongodb' => \denha\cache\drivers\Mongodb::class,
+        /** PReids缓存 */
+        'Predis'  => \denha\cache\drivers\Predis::class,
+        /** Reids缓存 */
+        'Redis'   => \denha\cache\drivers\Redis::class,
+    ];
+
     public static function message($config = [])
     {
 
-        $type = $config['type'] ?: 'File';
+        $type = $config['type'] ?? 'File';
         $id   = md5(json_encode($config));
 
+        if (!isset(self::$handlerClass[$type])) {
+            throw new Exception("Cache Handers Not Find :" . $type);
+        }
+
         if (!isset(self::$instance[$id])) {
-            $driversClass        = 'denha\cache\drivers\\' . $type;
-            self::$instance[$id] = new $driversClass($config);
+            self::$instance[$id] = new self::$handlerClass[$type]($config);
         }
 
         return self::$instance[$id];
